4478e80ece02b5a60a4a0afce6e85fed0516fc32
kinderwond <i.ndeilko@gmail.com>
2019-08-20 17:04:10 +0300

    added project

---
+++ app.js
@@ -0,0 +1,45 @@
+const Telegraf = require('telegraf'),
+ 	  LocalSession = require('telegraf-session-local'),
+	  fs = require('fs'),
+	  bot = new Telegraf(process.env.TOKEN),
+	  users = JSON.parse(fs.readFileSync("users.json", "utf-8"))
+	 
+var	id = 0
+const  property = 'data',
+	  localSession = new LocalSession({
+    	 database: 'db.json',
+  		property: 'session',
+  		storage: LocalSession.storageFileAsync,
+  		format: {
+    		serialize: (obj) => JSON.stringify(obj, null, "\t"),
+    		deserialize: (str) => JSON.parse(str),
+  		}
+	})
+
+bot.use(localSession.middleware())
+bot.use(async (ctx, next) => {
+	if (ctx.session.tomatos > 50) {
+		ctx.session.level = "Lover tomato"
+	}
+
+	if (ctx.session.auth == true) {
+		var x = require('./usage')(bot)
+		return next();
+	} else 
+		return ctx.reply('Добро пожаловать! Извините, но вы не зарегестрированы в системе! По поводу регистрации обракщатся к @kinderwond ' + ctx.from.id)		
+})
+
+bot.start( (ctx, next) => {
+	id = ctx.from.id
+  	for (let key in users) {
+    	if (users[key].id == id) {
+      		ctx.session.auth = true;
+      		return ctx.session.auth;
+    	}	
+  	}
+  	ctx.session.auth = false;
+  	return ctx.session.auth;
+})
+
+bot.launch()
+
