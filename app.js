8956221a83f83c0ebcc399a0f5cffe0dd612c1f3
kinderwond <44819377+kinderwond@users.noreply.github.com>
2019-12-13 12:55:06 +0200

    add id users for testing other people, edit authorization from userid to localStorage

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-
+require('dotenv').config()
 const Telegraf = require("telegraf")
 const LocalSession = require("./sessionConfig")
 const fs = require("fs")
 const http = require('http')
 
-const bot = new Telegraf("784954893:AAEAtfBTG8v2XQG8LNvT1eDssLH3_j0SLF0")
-const users = JSON.parse(fs.readFileSync("./db/usersId.json", "utf-8"))
+const bot = new Telegraf(process.env.token)
+const db = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"))
 const server = http.createServer()
 
-bot.use(LocalSession.middleware());
+bot.use(LocalSession.middleware())
 bot.use(async (ctx, next) => {
   if (ctx.session.tomatos > 50) 
     ctx.session.level = "Lover tomato";
@@ -18,15 +18,20 @@ bot.use(async (ctx, next) => {
     require("./handlers/weatherHandler.js")(bot)
     require("./handlers/scheduleHandlerr.js")(bot)
     return next();
-  } else
+  } else{
     return ctx.reply("Добро пожаловать! Извините, но вы не зарегестрированы в системе!" + 
-                     "По поводу регистрации обракщатся к @kinderwond");
+                     "По поводу регистрации обращатся к @kinderwond")
+  }
 });
 
-bot.hears('/start', ctx => {
+bot.hears('/start', (ctx) => {
   let id = ctx.from.id;
-  for (let key in users.usersID) {
-    if (users.usersID[key] === id) {
+  console.log(id)
+  for (let key in db.sessions) {
+    sessionID =  +(db.sessions[key].id.substring(0,9))
+    console.log(sessionID)
+    
+    if (sessionID === id) {
       ctx.session.auth = true;
       ctx.reply('Авторизация прошла успешно')
       return ctx.session.auth;
@@ -36,6 +41,7 @@ bot.hears('/start', ctx => {
 
   return ctx.session.auth;
 });
+
 
 bot.launch();
 server.listen(process.env.PORT)
